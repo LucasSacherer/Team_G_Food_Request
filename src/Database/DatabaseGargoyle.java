de57041b17604b15ecbef38dbd0034f76b598b37
Quinn Averill <qsaverill@wpi.edu>
2017-12-03 15:21:38 -0500

    Working on Database setup

---
+++ src/Database/DatabaseGargoyle.java
@@ -0,0 +1,73 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class DatabaseGargoyle {
+    final String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+    private Connection connection;
+    private Statement statement;
+    //private ArrayList<EntityManager> managers;
+
+    /**
+     * Loads the drivers for the databaswe connection, and then opens a connection to the DB with an open statement
+     */
+    public void createConnection() {
+        //Load driver
+        try {
+            Class.forName(driver).newInstance();
+        }catch (Exception ex){
+            System.out.println("Failed to find Embedded JavaDB driver!");
+            ex.printStackTrace();
+        }
+
+        //Create connection and statement to be run
+        try {
+            this.connection = DriverManager.getConnection("jdbc:derby:mini-app;create=true");
+            this.statement = connection.createStatement();
+        }catch (SQLException ex){
+            System.out.println("Exception thrown in createConnection()");
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Destroys the current connection to the database, as well as it's statement
+     */
+    public void destroyConnection() {
+        try {
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Exception thrown in destroyConnection()");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Creates all the tables in the database
+     */
+    public void createTables() {
+        TableCreator tableCreator = new TableCreator(this.statement);
+        tableCreator.createMenuItemTable(this.connection);
+        tableCreator.createNodeTable(this.connection);
+        tableCreator.createFoodRequestTable();
+        tableCreator.createFoodOrderTable();
+        tableCreator.createFoodLogTable();
+    }
+
+    public void executeUpdateOnDatabase(String sql) {
+
+    }
+
+    public ResultSet executeQueryOnDatabase(String sql) {
+        return null;
+    }
+
+}
--- src/FoodRequest.java
+++ src/FoodRequest.java
@@ -1,3 +1,4 @@
+import Database.DatabaseGargoyle;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,6 +9,13 @@ public class FoodRequest extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        //Load the Database
+        DatabaseGargoyle databaseGargoyle = new DatabaseGargoyle();
+        databaseGargoyle.createConnection();
+        databaseGargoyle.createTables();
+        databaseGargoyle.destroyConnection();
+
+        //Load the UI
         Parent root = FXMLLoader.load(getClass().getResource("Boundary/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
