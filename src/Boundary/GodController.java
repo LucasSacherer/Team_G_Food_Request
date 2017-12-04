74e8fdab1ce3dd187fedc465c4b3797a7fb9023b
Taylor Stephen <tastephen@wpi.edu>
2017-12-03 23:31:00 -0500

    Added all controllers, and buttons for the scenes.
    still need the on action button and scene switching

---
+++ src/Boundary/GodController.java
@@ -0,0 +1,40 @@
+package Boundary;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class GodController {
+
+
+    ///////////////////////
+    /** FXML Attributes **/
+    ///////////////////////
+    /* Edit Menu */
+
+    @FXML
+    private JFXButton editMenuBack;
+
+    /* Request Hub */
+    /* Map Directory */
+    /* Reports */
+
+    @FXML
+    private JFXButton reportsBack;
+
+    /* Staff Info Popup */
+    /* Staff Menu Order */
+
+    @FXML
+    private JFXButton menuOrderBack;
+
+    /** Organize Functions by Scene **/
+
+
+    private void requestToHub(ActionEvent e) { }
+
+    ////////////////
+    /* Main scene */
+    ////////////////
+
+}
