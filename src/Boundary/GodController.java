b1456a7e42c9ddf636d675b99a430e667010b408
dtsiakmakis <33322535+dtsiakmakis@users.noreply.github.com>
2017-12-04 00:32:27 -0500

    Scene Switching from Hub to adminEdit,foodRequest, reports and back Basic God Controller Setup Food Request starts up at main Hub

--- src/Boundary/GodController.java
+++ src/Boundary/GodController.java
@@ -1,29 +1,71 @@
 package Boundary;
 
+import Database.DatabaseGargoyle;
+import Entity.FoodRequest;
+import Entity.MenuItem;
+import Entity.Worker;
+import Manager.*;
 import com.jfoenix.controls.JFXButton;
+import com.sun.xml.internal.bind.v2.TODO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+import java.nio.file.Path;
 
 public class GodController {
+    /* Database Gargoyle */
+    final private DatabaseGargoyle databaseGargoyle = new DatabaseGargoyle();
+
+    /* Managers */
+    FoodLogManager foodLogManager = new FoodLogManager(databaseGargoyle);
+    MenuItemManager menuItemManager = new MenuItemManager(databaseGargoyle);
+    NodeManager nodeManager = new NodeManager(databaseGargoyle);
+    WorkerManager workerManager = new WorkerManager(databaseGargoyle);
+    FoodRequestManager foodRequestManager = new FoodRequestManager( databaseGargoyle,  nodeManager,
+             workerManager,  menuItemManager,  foodLogManager);
+
+    /* Entities */
+
+    /* Scene Switcher */
+    SceneSwitcher sceneSwitcher = new SceneSwitcher();
 
 
     ///////////////////////
     /** FXML Attributes **/
     ///////////////////////
     /* Edit Menu */
+    @FXML
+    private Pane adminEditPane;
 
     @FXML
     private JFXButton editMenuBack;
 
     /* Request Hub */
+    @FXML
+    private Pane foodRequestHubPane;
+
+    @FXML
+    private JFXButton toAdminEdit, toStaffMenuOrder, toReports;
     /* Map Directory */
+
+    //TODO
+
     /* Reports */
+    @FXML
+    private Pane reportPane;
 
     @FXML
     private JFXButton reportsBack;
 
     /* Staff Info Popup */
+
+    //TODO
+
     /* Staff Menu Order */
+    @FXML
+    private Pane staffMenuOrderPane;
 
     @FXML
     private JFXButton menuOrderBack;
@@ -31,7 +73,24 @@ public class GodController {
     /** Organize Functions by Scene **/
 
 
-    private void requestToHub(ActionEvent e) { }
+
+    @FXML
+    private void staffMenuOrderToHub() throws IOException { sceneSwitcher.toFoodRequestHub(this, staffMenuOrderPane); }
+
+    @FXML
+    private void adminEditToHub() throws IOException { sceneSwitcher.toFoodRequestHub(this, adminEditPane); }
+
+    @FXML
+    private void reportsToHub() throws IOException { sceneSwitcher.toFoodRequestHub(this, reportPane); }
+
+    @FXML
+    private void hubToStaffMenuOrder() throws IOException { sceneSwitcher.toStaffMenuOrder(this, foodRequestHubPane); }
+
+    @FXML
+    private void hubToAdminEdit() throws IOException { sceneSwitcher.toAdminEditMenu(this, foodRequestHubPane); }
+
+    @FXML
+    private void hubToReports() throws IOException { sceneSwitcher.toReports(this, foodRequestHubPane); }
 
     ////////////////
     /* Main scene */
--- src/FoodRequest.java
+++ src/FoodRequest.java
@@ -1,3 +1,4 @@
+import Boundary.GodController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,9 +9,17 @@ public class FoodRequest extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Boundary/fxml/foodRequestHub.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+//        Parent root = FXMLLoader.load(getClass().getResource("Boundary/fxml/foodRequestHub.fxml"));
+//        primaryStage.setTitle("Hello World");
+//        primaryStage.setScene(new Scene(root, 300, 275));
+//        primaryStage.show();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("boundary/fxml/foodRequestHub.fxml"));
+        GodController godController = new GodController();
+        loader.setController(godController);
+        Parent root = loader.load();
+        primaryStage.setTitle("B&W Path Finding");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setMaximized(true);
         primaryStage.show();
     }
 
