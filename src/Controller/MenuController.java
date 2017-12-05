51584efe9e46ac81009914c6738612a0b0322e72
Niccki Franco <ncfranco@wpi.edu>
2017-12-05 14:59:43 -0500

    added option to get list of (diabetic,vegan, or gluten free)

--- src/Controller/MenuController.java
+++ src/Controller/MenuController.java
@@ -3,6 +3,7 @@ package Controller;
 import Entity.MenuItem;
 import Manager.MenuItemManager;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MenuController {
@@ -12,7 +13,47 @@ public class MenuController {
     }
 
     public List<MenuItem> getAvailableMenu(){
-        return mim.getMenuItems();
+        List<MenuItem> items = mim.getMenuItems();
+        List<MenuItem> avaiable = new ArrayList<>();
+        for(MenuItem mi: items){
+            if (mi.getStockAvailable() != 0){
+                avaiable.add(mi);
+            }
+        }
+        return avaiable;
+    }
+
+    public List<MenuItem> getDiabetic(){
+        List<MenuItem> items = mim.getMenuItems();
+        List<MenuItem> diabetic = new ArrayList<>();
+        for(MenuItem mi:items){
+            if(mi.getDiabetic()){
+                diabetic.add(mi);
+            }
+        }
+        return diabetic;
+    }
+
+    public List<MenuItem> getvegan(){
+        List<MenuItem> items = mim.getMenuItems();
+        List<MenuItem> vegan = new ArrayList<>();
+        for(MenuItem mi:items){
+            if(mi.getVegan()){
+                vegan.add(mi);
+            }
+        }
+        return vegan;
+    }
+
+    public List<MenuItem> getGultenFree(){
+        List<MenuItem> items = mim.getMenuItems();
+        List<MenuItem> gf = new ArrayList<>();
+        for(MenuItem mi:items){
+            if(mi.getGluttenFree()){
+                gf.add(mi);
+            }
+        }
+        return gf;
     }
 
     public void modifyMenuItem(MenuItem modified){
@@ -20,5 +61,9 @@ public class MenuController {
         if(mim.getMenuItemByName(modified.getFoodName()) != null) {
             mim.modifyMenuItem(modified);
         }
+    }
+
+    public void addMenuItem(MenuItem newItem){
+          //call addMenuItem from mim
     }
 }
