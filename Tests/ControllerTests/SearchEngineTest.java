be97dfe21ee5552c1c8d361474787415a819ec26
Niccki Franco <ncfranco@wpi.edu>
2017-12-05 14:19:40 -0500

    added test to make sure it can run twice in a row

--- Tests/ControllerTests/SearchEngineTest.java
+++ Tests/ControllerTests/SearchEngineTest.java
@@ -54,7 +54,34 @@ public class SearchEngineTest {
 
         System.out.println(names);
 
+    }
+
+    @Test
+    public void twice(){
+        DatabaseGargoyle databaseGargoyle = new DatabaseGargoyle();
+        FoodLogManager foodLogManager = new FoodLogManager(databaseGargoyle);
+        NodeManager nodeManager = new NodeManager(databaseGargoyle);
+        MenuItemManager menuItemManager = new MenuItemManager(databaseGargoyle);
+        databaseGargoyle.attachManager(foodLogManager);
+        databaseGargoyle.attachManager(nodeManager);
+        databaseGargoyle.attachManager(menuItemManager);
+        databaseGargoyle.notifyManagers();
+        SearchEngine se = new SearchEngine(nodeManager);
+        List<Node> answer = (se.Search("cafe"));
+        List<String> names = new ArrayList<>();
+        for(Node n: answer){
+            names.add(n.getShortName());
+        }
+
+        System.out.println(names);
 
 
+        List<Node> answer2 = (se.Search("Cafe"));
+        List<String> names2 = new ArrayList<>();
+        for(Node n: answer2){
+            names2.add(n.getShortName());
+        }
+
+        System.out.println(names2);
     }
 }
