f7bb2b4f939f218800a8ce309999adbfd5fe446b
Niccki Franco <ncfranco@wpi.edu>
2017-12-05 19:11:37 -0500

    fixed worker

--- src/Controller/WorkerController.java
+++ src/Controller/WorkerController.java
@@ -3,11 +3,14 @@ package Controller;
 import Entity.Worker;
 import Manager.WorkerManager;
 
+import java.util.List;
+
 public class WorkerController {
     WorkerManager wm;
     public WorkerController(WorkerManager wm){
         this.wm = wm;
     }
+
     public void modifyWorker(Worker modified){
         //check that it is a valid worker
         if(wm.getWorkerByID(modified.workerID)!= null) {
@@ -23,7 +26,11 @@ public class WorkerController {
 
     public void removeWorker(Worker fired){
         if(wm.getWorkerByID(fired.workerID)!= null){
-            removeWorker(fired);
+            wm.removeWorker(fired);
         }
+    }
+
+    public List<Worker> getWorkers(){
+        return wm.getWorkers();
     }
 }
